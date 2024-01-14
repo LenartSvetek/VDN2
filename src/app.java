862da55977ff6282468bc6838bdad8916756e94a
Lenart Svetek <never.saymeme@gmail.com>
2024-01-14 21:12:08 +0100

    tst

--- src/app.java
+++ src/app.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class app{
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
         new GameWindow();
     }
 }
